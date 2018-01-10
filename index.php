ca73956541c982830c06918e35b75d4a4a80458c
NuttawetNaja <35288854+NuttawetNaja@users.noreply.github.com>
2018-01-11 00:29:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -80,4 +80,32 @@ curl_close ($ch);
  
  echo "OK";
 ?>
+<?php
+ 
+$strAccessToken = "qveMi/HKFOa5gbemLBlyMo2AubnOw0PTQw8C2Y8aOCYJnXqlIYzE7sIhD/5ikBAeTtvqyya/oifbYN9ND3QMieDvXAFb+cusVdRsSRgoYL5o50L7/weYoLVOxdSxdpwaMnDcK9WR0oDIHl8ayyEKrQdB04t89/1O/w1cDnyilFU=";
+ 
+$strUrl = "https://api.line.me/v2/bot/message/push";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+$arrPostData = array();
+$arrPostData['to'] = "U165949810b33ecbd4e1228593b69d954";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
 
